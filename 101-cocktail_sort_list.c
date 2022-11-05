f225dca00a20a1d12a9b1169dbe8a292d4b95f7f
Yirga Mulaw <infoyirga12@gmail.com>
2022-11-05 18:56:16 +0300

    Cocktail sort

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -2,10 +2,11 @@
 
 /**
  * swap_node_ahead - Swap a node in a listint_t doubly-linked list
- *                   list of integers with the node ahead of it.
+ * **list of integers with the node ahead of it.
  * @list: A pointer to the head of a doubly-linked list of integers.
  * @tail: A pointer to the tail of the doubly-linked list.
- * @shaker: A pointer to the current swapping node of the cocktail shaker algo.
+ * @shaker: A pointer to the current swapping node of
+ * **the cocktail shaker algorithm.
  */
 void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **shaker)
 {
@@ -28,10 +29,11 @@ void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **shaker)
 
 /**
  * swap_node_behind - Swap a node in a listint_t doubly-linked
- *                    list of integers with the node behind it.
+ * **list of integers with the node behind it.
  * @list: A pointer to the head of a doubly-linked list of integers.
  * @tail: A pointer to the tail of the doubly-linked list.
- * @shaker: A pointer to the current swapping node of the cocktail shaker algo.
+ * @shaker: A pointer to the current swapping 
+ * **node of the cocktail shaker algorithm.
  */
 void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker)
 {
@@ -53,8 +55,8 @@ void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker)
 }
 
 /**
- * cocktail_sort_list - Sort a listint_t doubly-linked list of integers in
- *                      ascending order using the cocktail shaker algorithm.
+ * cocktail_sort_list - Sort a listint_t doubly-linked list of 
+ * *integers in ascending order using the cocktail shaker algorithm.
  * @list: A pointer to the head of a listint_t doubly-linked list.
  */
 void cocktail_sort_list(listint_t **list)
--- sort.h
+++ sort.h
@@ -40,6 +40,5 @@ void lomuto_sort(int *array, size_t size, int left, int right);
 void quick_sort(int *array, size_t size);
 void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **shaker);
 void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker);
-void cocktail_sort_list(listint_t **list);
 
 #endif /* SORT_H */
