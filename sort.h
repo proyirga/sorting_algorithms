2838a434600e5ceca042f8d3da61b42f371b027c
Yirga Mulaw <infoyirga12@gmail.com>
2022-11-05 18:49:01 +0300

    Cocktail sort

--- sort.h
+++ sort.h
@@ -28,6 +28,7 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
 
 /**
  * User defined function prototypes
@@ -37,5 +38,8 @@ void swap_ints(int *x, int *y);
 int lomuto_partition(int *array, size_t size, int left, int right);
 void lomuto_sort(int *array, size_t size, int left, int right);
 void quick_sort(int *array, size_t size);
+void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **shaker);
+void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker);
+void cocktail_sort_list(listint_t **list);
 
 #endif /* SORT_H */
