61773ec8ebc2b969fc1e33d096da994610773076
Yirga Mulaw <infoyirga12@gmail.com>
2022-11-07 11:11:16 +0300

    Radix Sort

--- sort.h
+++ sort.h
@@ -35,6 +35,7 @@ void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
 
 /**
  * User defined function prototypes
