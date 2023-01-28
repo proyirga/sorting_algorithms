30601aec1be11c49f95f4d1866b11f52e23d5752
Yirga Mulaw <infoyirga12@gmail.com>
2023-01-28 10:26:23 +0300

    Bubble 1 line added

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,16 +13,18 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 
-	for (i = 0; i <= size - 1; i++)
+	int tmp;
+
+	for (i = 0; i <= size; i++)
 	{
-		for (j = 0; j <= (size - i) - 1; j++)
+		for (j = 0; j <= size - 2; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				int temp = array[j];
+				tmp = array[j];
 
 				array[j] = array[j + 1];
-				array[j + 1] = temp;
+				array[j + 1] = tmp;
 				print_array(array, size);
 			}
 		}
