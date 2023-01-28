6d9fa1b38d96c27104e9c185e561b13764a6e859
Yirga Mulaw <infoyirga12@gmail.com>
2023-01-28 12:22:27 +0300

    Selection

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,36 +1,45 @@
 #include "sort.h"
 
 /**
- * selection_sort - sorts an array of integers in 
- * *ascending order using the Selection sort algorithm
- * @array: given array
- * @size: given size of the array
+ * swap - Swap two integers in an array.
+ * @arr_n: The first integer to swap.
+ * @arr_n1: The second integer to swap.
  */
+void swap(int *arr_n, int *arr_n1)
+{
+	int tmp;
 
+	tmp = *arr_n;
+	*arr_n = *arr_n1;
+	*arr_n1 = tmp;
+}
+
+/**
+ * selection_sort - Sort an array of integers in ascending order
+ *                  using the selection sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Prints the array after each swap.
+ */
 void selection_sort(int *array, size_t size)
 {
+	int *min;
 	size_t i, j;
-	int min_index;
 
 	if (array == NULL || size < 2)
 		return;
 
-	for (i = 0; i <= size-2; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		min_index = i;
-		for (j = i + 1; j <= size-1; j++)
-		{
-			if (array[j] < array[min_index])
-				min_index = j;
-		}
+		min = array + i;
+		for (j = i + 1; j < size; j++)
+			min = (array[j] < *min) ? (array + j) : min;
 
-		if (min_index != i)
+		if ((array + i) != min)
 		{
-			int temp;
-                        temp = array[min_index];
-                        array[min_index] = array[i];
-                        array[i] = temp;
-	                print_array(array, size);
-                }
+			swap_ints(array + i, min);
+			print_array(array, size);
+		}
 	}
 }
