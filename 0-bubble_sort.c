e5f5d4e9c20fa754f5075e72b1507a78f8ad696b
Yirga Mulaw <infoyirga12@gmail.com>
2023-01-28 11:09:45 +0300

    swap + bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -28,7 +28,7 @@ void bubble_sort(int *array, size_t size)
 	for (i = 0; i <= size; i++)
 	{
 		if (array == NULL || size < 2)
-			break;
+			return;
 		for (j = 0; j <= size - 2; j++)
 		{
 			if (array[j] > array[j + 1])
