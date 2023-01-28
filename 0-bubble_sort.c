a96c4473336c573fc02ad1ed2840e2db037695cb
Yirga Mulaw <infoyirga12@gmail.com>
2023-01-28 10:00:33 +0300

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,17 +11,16 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t i;
+	size_t i, j;
 
-	for (i = 0; i <= size-1; i++)
+	for (i = 0; i <= size - 1; i++)
 	{
-		size_t j;
-
-		for (j = 0; j <= size-2; j++)
+		for (j = 0; j <= size - 2; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				int temp = array[j];
+
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 				print_array(array, size);
